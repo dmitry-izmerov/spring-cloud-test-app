bbc9a8d8dd51219d9d47d8dac2a6ef341197cb5c
demi <idd90i@gmail.com>
2018-06-03 15:49:19 +0300

    changed room service module stucture and added reservation module

---
+++ reservation-service/src/main/java/ru/demi/services/reservation/ServicesReservationApplication.java
@@ -0,0 +1,12 @@
+package ru.demi.services.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesReservationApplication.class, args);
+    }
+}
